a70e638395e8182aadff0b401e11b365660f6a23
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2016-03-18 14:01:35 +0000

    d length check

--- 68714/time/attack.c
+++ 68714/time/attack.c
@@ -224,8 +224,7 @@ mpz_t R;mpz_init(R);
       gmp_randclear(state);
       mpz_set_ui(cZ, 0);
       while(mpz_cmp(cZ, Z3)<=0){
-        srand(time(NULL));
-        random = rand();
+        //random = rand();
         printf("%lu\n", random);
         gmp_randseed_ui(state, random);
         mpz_urandomm(cZ, state, Z2);
