e6b11ea16422b99477f669d91eed7a4b04086059
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2016-03-18 05:56:59 +0000

    d length check

--- 68714/time/attack.c
+++ 68714/time/attack.c
@@ -120,6 +120,7 @@ void find_N2(mpz_t N2, const mpz_t N, const mpz_t R){
   mpz_gcdext(tmp, rInv, N2, R, N);
   mpz_mul(tmp2, R, rInv);
   mpz_mul(tmp3, N, N2);
+  gmp_printf("gcd %ZX\n%ZX\n", tmp2, tmp3);
   mpz_sub(tmp2, tmp2, tmp3);
   gmp_printf("gcd %ZX\n%ZX\n", tmp, tmp2);
   mpz_clear(tmp);
