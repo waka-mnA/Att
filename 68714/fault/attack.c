e672aa94fba2e733fb63f17c468174461c7fd986
waka-mnA <cprc_lr_hysks@yahoo.co.jp>
2016-04-30 14:44:15 +0100

    test

--- 68714/fault/attack.c
+++ 68714/fault/attack.c
@@ -7,93 +7,76 @@
 #define BUFFER_SIZE ( 80 )
 
 pid_t pid        = 0;    // process ID (of either parent or child) from fork
-pid_t pid_R        = 0;    // process ID (of either parent or child) from fork
 
 int   target_raw[ 2 ];   // unbuffered communication: attacker -> attack target
 int   attack_raw[ 2 ];   // unbuffered communication: attack target -> attacker
-int   target_R_raw[ 2 ];   // unbuffered communication: attacker -> R
-int   attack_R_raw[ 2 ];   // unbuffered communication: R -> attacker
 
 FILE* target_out = NULL; // buffered attack target input  stream
 FILE* target_in  = NULL; // buffered attack target output stream
-FILE* R_out = NULL; // buffered attack R input  stream
-FILE* R_in  = NULL; // buffered attack R output stream
 
 FILE* data_in  = NULL; //.conf file
 
 int interaction= 0;
-
-void interact( int* t, mpz_t m, const mpz_t c){
-  //Send c
-  gmp_fprintf(target_in, "%ZX\n", c); fflush(target_in);
-  //Receive execution time and plaintext from target
-  if ( 1 != fscanf(target_out, "%d", t)){ abort(); }
-  if (gmp_fscanf(target_out, "%ZX", m) == 0) { abort(); }
+//Function to generate fault specification
+//if first argument is negative, it means no fault induced
+char* faultSpec( const int r, const int f, const int p, const int i, const int j){
+  if (r < 0) return "";
+  int size = 9;
+  if (r > 9) size++;
+  char result[size];
+  strcpy(result, (char) r); //r = {0-10}
+  strcat(result, (char) f); //f = {0-3}
+  strcat(result, (char) p); //p = {0,1}
+  strcat(result, (char) i); //i = {0-3}
+  strcat(result, (char) j); //j = {0-3}
+  return result;
+}
+void interact(  mpz_t c, const char* spec, const mpz_t m){
+  //Send spec and m
+  gmp_fprintf(target_in, "%s\n", spec); fflush(target_in);
+  gmp_fprintf(target_in, "%ZX\n", m); fflush(target_in);
+  //Receive c from target
+  if (gmp_fscanf(target_out, "%ZX", c) == 0) { abort(); }
   interaction++;
 }
 
-void interact_R( int* t, mpz_t m, const mpz_t c, const mpz_t N, const mpz_t d){
-  //Send c, N, d
-  gmp_fprintf(R_in, "%ZX\n", c); fflush(R_in);
-  gmp_fprintf(R_in, "%ZX\n", N); fflush(R_in);
-  gmp_fprintf(R_in, "%ZX\n", d); fflush(R_in);
-  //Receive execution time and plaintext from target
-  if ( 1 != fscanf(R_out, "%d", t)){ abort(); }
-  if (gmp_fscanf(R_out, "%ZX", m) == 0) { abort(); }
-  interaction++;
+//Convert integer to octet string
+char* int2oct(const mpz_t i, const mpz_t N){
+  char* octet = NULL;
+  int size = mpz_sizeinbase(N, 16);
+  int l = mpz_sizeinbase(i, 16);
+  octet = malloc(size+1);
+
+  char* tmpStr = NULL;
+  tmpStr = mpz_get_str(tmpStr, 16, i);
+  int index = 0;
+  for (index = 0; index<=(size - l) ;index++){
+    octet[index] = '0';
+  }
+  int m = 0;
+  for (int k = index-1;k<size;k = k+1){
+    octet[k] = toupper(tmpStr[m]);
+    m++;
+  }
+  octet[size] = '\0';
+  return octet;
 }
 
-void find_R(mpz_t R, const mpz_t N){
-  mpz_set_ui(R, 1);
-  int length = mpz_sizeinbase(N, 2);//N bit size
-  int lengthR = length%64;
-  if (lengthR!= 0) length= length +(64 - lengthR)-1;
-  mpz_mul_2exp(R, R, length);
+//Convert octet string to integer
+void oct2int(mpz_t i, const char* string){
+  mpz_set_str(i, string, 16);
 }
 
-//N * NR = -1 MOD R
-void find_N2(mpz_t N2, mpz_t rInv, const mpz_t N, const mpz_t R){
-  mpz_t tmp; mpz_init(tmp);
-  mpz_gcdext(tmp, rInv, N2, R, N);
-  mpz_mul_si(N2, N2, -1);
-  mpz_clear(tmp);
-}
-//return true if u>=N, (with reduction), false if not
-int monPro(const mpz_t a, const mpz_t b, const mpz_t N, const mpz_t N2, const mpz_t R){
-  mpz_t t;mpz_init(t);
-  mpz_t a2;mpz_init(a2);
-  mpz_t b2;mpz_init(b2);
-  mpz_t u;mpz_init(u);
-  mpz_t tmp;mpz_init(tmp);
-
-  //Montgomery a
-  mpz_mul(a2, a, R);
-  mpz_mod(a2, a2, N);
-  //Montgomery b
-  mpz_mul(b2, b, R);
-  mpz_mod(b2, b2, N);
-  mpz_mul(t, a2, b2);
-  //calc u = (t + (t*N2 mod R)N)/R
-  mpz_mul(tmp, t, N2);
-  mpz_mod(tmp, tmp, R);
-  mpz_mul(tmp, tmp, N);//(t*N2 mod R)N
-  mpz_add(tmp, t, tmp);
-  mpz_div(u, tmp, R);
-  if (mpz_cmp(u, N)<0) return false;
-  return true;
-
-  mpz_clear(t); mpz_clear(tmp);
-  mpz_clear(u); mpz_clear(a2);
-  mpz_clear(b2);
-}
 
 //mpz_t N, e, ...
 void attack() {
-  mpz_t N;      mpz_init(N);
-  mpz_t e;      mpz_init(e);
   mpz_t m;      mpz_init(m);
   mpz_t c;      mpz_init(c);
-  mpz_t m_R;    mpz_init(m_R);
+  mpz_t cF;      mpz_init(cF);//with fault
+
+  //Unused variables
+  mpz_t N;      mpz_init(N);
+  mpz_t e;      mpz_init(e);
   mpz_t cY;     mpz_init(cY);
   mpz_t cZ;     mpz_init(cZ);
   mpz_t dFinal; mpz_init(dFinal);//Final Target Material
@@ -104,17 +87,6 @@ void attack() {
   mpz_t cTmp;   mpz_init(cTmp);  //mtmp
   mpz_t cTmpC;  mpz_init(cTmpC);//mtmp * m
   mpz_t dTmp;   mpz_init(dTmp);
-  int r_R = 0;
-
-  //Read N and e from conf file
-  if (gmp_fscanf(data_in, "%ZX", N) == 0) { abort(); }
-  if (gmp_fscanf(data_in, "%ZX", e) == 0) { abort(); }
-  fclose(data_in);
-
-  //Find R and N' for Montgomery reduction
-  find_R(R, N);
-  find_N2(N2,rInv, N, R);
-
   int yAvg1, zAvg1, yAvg2, zAvg2; //time average for each ciphertext set
   int yNum1, zNum1, yNum2, zNum2; //number of ciphertexts in each set
   int tY, tZ;
@@ -124,6 +96,13 @@ void attack() {
   int endFlag = 0;
   int j = 1;    //bit number
 
+
+  char* pt ="3243F6A8885A308D313198A2E0370734";
+
+  mpz_t test; mpz_init(test);
+  oct2int(test, pt);
+  gmp_printf("TEST %ZX\n", test);
+  gmp_printf("TEST %Zd\n", test);
   char dChar[1024];
   dChar[0]='1';
   srand(time(NULL));
@@ -244,23 +223,16 @@ void cleanup( int s ){
   // Close the   buffered communication handles.
   fclose( target_in  );
   fclose( target_out );
-  fclose( R_in  );
-  fclose( R_out );
+
   // Close the unbuffered communication handles.
   close( target_raw[ 0 ] );
   close( target_raw[ 1 ] );
   close( attack_raw[ 0 ] );
   close( attack_raw[ 1 ] );
-  close( target_R_raw[ 0 ] );
-  close( target_R_raw[ 1 ] );
-  close( attack_R_raw[ 0 ] );
-  close( attack_R_raw[ 1 ] );
+
   // Forcibly terminate the attack target process.
   if( pid > 0 ) {
     kill( pid, SIGKILL );
-  }
-  if( pid_R > 0 ) {
-    kill( pid_R, SIGKILL );
   }
 
   // Forcibly terminate the attacker      process.
@@ -305,38 +277,6 @@ int main( int argc, char* argv[] ) {
       }
 
       default : {
-        if( pipe( target_R_raw ) == -1 ) {
-          abort();
-        }
-        if( pipe( attack_R_raw ) == -1 ) {
-          abort();
-        }
-
-        switch(pid_R = fork()){
-          case -1 : {
-            // The fork failed; reason is stored in errno, but we'll just abort.
-            abort();
-          }
-
-          case +0 : {
-            // (Re)connect standard input and output to pipes.
-            close( STDOUT_FILENO );
-            close(  STDIN_FILENO );
-            if( dup2( attack_R_raw[ 1 ], STDOUT_FILENO ) == -1 ) {
-              abort();
-            }
-
-            if( dup2( target_R_raw[ 0 ],  STDIN_FILENO ) == -1 ) {
-              abort();
-            }
-            // Produce a sub-process representing the attack target.
-            execl( "68714.R", argv[ 0 ], NULL );
-
-            // Break and clean-up once finished.
-            break;
-          }
-
-        default : {
           // Construct handles to attack target standard input and output.
           if( ( target_out = fdopen( attack_raw[ 0 ], "r" ) ) == NULL ) {
             abort();
@@ -344,21 +284,10 @@ int main( int argc, char* argv[] ) {
           if( ( target_in  = fdopen( target_raw[ 1 ], "w" ) ) == NULL ) {
             abort();
           }
-          if( ( R_out = fdopen( attack_R_raw[ 0 ], "r" ) ) == NULL ) {
-            abort();
-          }
-          if( ( R_in  = fdopen( target_R_raw[ 1 ], "w" ) ) == NULL ) {
-            abort();
-          }
-          if ((data_in = fopen(argv[2], "r"))== NULL){
-            abort();
-          }//
+
           // Execute a function representing the attacker.
           attack();
 
-          // Break and clean-up once finished.
-          break;
-        }
         // Break and clean-up once finished.
         break;
       }
