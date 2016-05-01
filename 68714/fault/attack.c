beb7059cdb8ef2ec09712413d481f36cbba8e278
waka-mnA <cprc_lr_hysks@yahoo.co.jp>
2016-05-01 12:46:12 +0100

    fault free

--- 68714/fault/attack.c
+++ 68714/fault/attack.c
@@ -178,12 +178,15 @@ gmp_printf("4 S1: %ZX\n", c);
     while(solved == 0 && i<256){
       int delta1 =inv_s[x[0]^i]^inv_s[y[0]^i];
       gmp_printf("1 %d\n", delta1);
+      j=0;
       while((delta1 == delta*2)&&(solved == 0) && j<256){
         int delta11 = inv_s[x[10]^j]^inv_s[y[10]^j];
         gmp_printf("11 %d\n", delta11);
+        z=0;
           while((delta11 == delta)&&(solved == 0) && z<256){
             int delta14 = inv_s[x[13]^z]^inv_s[y[13]^z];
             gmp_printf("14 %d\n", delta14);
+            l=0;
               while((delta14 == delta)&&(solved==0) && l<256){
                 int delta8 = inv_s[x[7]^l]^inv_s[y[8]^l];
                 gmp_printf("8 %d\n", delta8);
