b831691f6b58f6db47fcfb7513a87d6e9bfd175a
waka-mnA <cprc_lr_hysks@yahoo.co.jp>
2016-05-03 18:39:12 +0100

    comparekey

--- 68714/fault/attack.c
+++ 68714/fault/attack.c
@@ -216,14 +216,10 @@ int findSolution(int* keyArray, int x1, int x2, int x3, int x4, int*x, int*y, in
           printf("%d\n", x2);
           printf("%d\n", x3);
           printf("%d\n", x4);
-          printf("%d\n", x_2[x1]);
+          printf("%d\n", x_2[0]);
           printf("%d\n", x_2[x2]);
           printf("%d\n", x_2[x3]);
           printf("%d\n", x_2[x4]);
-          printf("%d\n", y_2[x1]);
-          printf("%d\n", y_2[x2]);
-          printf("%d\n", y_2[x3]);
-          printf("%d\n", y_2[x4]);
     findK1(x_2[x1], x_2[x2], x_2[x3], x_2[x4],
       y_2[x1], y_2[x2], y_2[x3], y_2[x4],
       keySto1_2, keySto2_2, keySto3_2, keySto4_2 );
