5ba9687bbbd592c7ccc28615cd25455aea346b20
waka-mnA <cprc_lr_hysks@yahoo.co.jp>
2016-05-03 15:55:59 +0100

    comparekey

--- 68714/fault/attack.c
+++ 68714/fault/attack.c
@@ -297,8 +297,8 @@ int step1(mpz_t c, mpz_t c2, int* keyArray){
 
 
   findK1(x[0], x[7], x[10], x[13], y[0], y[7], y[10], y[13], k1, k8, k11, k14 );
-  findK1(x[1], x[4], x[14], x[11], y[1], y[4], y[14], y[11], k2, k5, k15, k12);
-//  findK1(x[2], x[5], x[15], x[8],y[2], y[5], y[15], y[8],  k3, k6, k16, k9);
+//  findK1(x[1], x[4], x[14], x[11], y[1], y[4], y[14], y[11], k2, k5, k15, k12);
+  findK1(x[2], x[5], x[15], x[8],y[2], y[5], y[15], y[8],  k3, k6, k16, k9);
 //  findK1(x[9], x[12], x[3], x[6],  y[9], y[12], y[3], y[6],  k10, k13, k4, k7);
 /*
   findK1(x[11], x[14], x[4], x[1], y[11], y[14], y[4], y[1], k12, k15, k5, k2);
@@ -310,8 +310,8 @@ int step1(mpz_t c, mpz_t c2, int* keyArray){
   }*/
   printf("First analysis end\n");
   interact(cF2, fault, pt2);
-  gmp_printf("4 S1: %ZX\n", c2);
-  gmp_printf("4 S1: %ZX\n", cF2);
+  //gmp_printf("4 S1: %ZX\n", c2);
+  //gmp_printf("4 S1: %ZX\n", cF2);
 
   char* ct2 = int2oct(c2);
   char* ctF2 = int2oct(cF2);
@@ -323,8 +323,8 @@ int step1(mpz_t c, mpz_t c2, int* keyArray){
   convertToIntArray(x, ct2);
   convertToIntArray(y, ctF2);
   findK1(x[0], x[7], x[10], x[13], y[0], y[7], y[10], y[13], k1_2, k8_2, k11_2, k14_2 );
-  findK1(x[1], x[4], x[14], x[11], y[1], y[4], y[14], y[11], k2_2, k5_2, k15_2, k12_2);
-//  findK1(x[2], x[5], x[15], x[8], y[2], y[5], y[15], y[8], k3_2, k6_2, k16_2, k9_2);
+//  findK1(x[1], x[4], x[14], x[11], y[1], y[4], y[14], y[11], k2_2, k5_2, k15_2, k12_2);
+  findK1(x[2], x[5], x[15], x[8], y[2], y[5], y[15], y[8], k3_2, k6_2, k16_2, k9_2);
 //  findK1(x[9], x[12], x[3], x[6], y[9], y[12], y[3], y[6], k10_2, k13_2, k4_2, k7_2);
   /*for (int i = 0;i<index2;i++){
     gmp_printf("index %d %d %d %d\n", k1_2[i], k8_2[i], k11_2[i], k14_2[i]);
