ae97f60d04f0c259d092b9868dfc805bc4edea8c
waka-mnA <cprc_lr_hysks@yahoo.co.jp>
2016-05-04 13:53:41 +0100

    power test

--- 68714/fault/attack.c
+++ 68714/fault/attack.c
@@ -268,7 +268,7 @@ int step(mpz_t c, mpz_t c2){
   int keyTest = 1;
   keyTest = keyTest & (findSolution(0, 7, 10, 13));
   keyTest = keyTest & (findSolution(11, 14, 1, 4));
-  keyTest = keyTest & (findSolution(8, 15, 2, 5));
+  keyTest = keyTest & (findSolution(15, 8, 2, 5));
   keyTest = keyTest & (findSolution(9, 12, 3, 6));
 //  keyTest = keyTest & (findSolution(11, 14, 1, 4));
 //  keyTest = keyTest & (findSolution(2, 5, 15, 8));
