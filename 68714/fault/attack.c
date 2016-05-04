b0e3e39e05c626e96dc9e23c752bb99a9e834e9c
waka-mnA <cprc_lr_hysks@yahoo.co.jp>
2016-05-04 14:18:47 +0100

    power test

--- 68714/fault/attack.c
+++ 68714/fault/attack.c
@@ -266,10 +266,10 @@ int step(mpz_t c, mpz_t c2){
   //Find keys by solving equations.
   //Check every function found only one solution
   int keyTest = 1;
-//0  keyTest = keyTest & (findSolution(0, 7, 10, 13));
-//  keyTest = keyTest & (findSolution(11, 14, 1, 4));
+  keyTest = keyTest & (findSolution(0, 7, 10, 13));
+  keyTest = keyTest & (findSolution(11, 14, 1, 4));
   keyTest = keyTest & (findSolution(2, 5, 15, 8));
-//  keyTest = keyTest & (findSolution(9, 12, 3, 6));
+  keyTest = keyTest & (findSolution(9, 12, 3, 6));
 //  keyTest = keyTest & (findSolution(11, 14, 1, 4));
 //  keyTest = keyTest & (findSolution(2, 5, 15, 8));
 //  keyTest = keyTest & (findSolution(9, 12, 3, 6));
