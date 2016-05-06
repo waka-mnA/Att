4af8442bc084d83ab82a36950dd64c2fc26980a9
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2016-05-06 12:47:20 +0100

    power

--- 68714/power/attack.c
+++ 68714/power/attack.c
@@ -287,12 +287,12 @@ void attack() {
           s_sq_X+= (double) (h[i][ki]* h[i][ki]);
           s_sq_T+= (double)(t[i][j]* t[i][j]);
         }
-        //Correlation coefficient with magnification of 100
-        R[j] = 100*(M_SIZE*s_HT - s_H*s_T)/(sqrt((M_SIZE*s_sq_X - s_H*s_H)*(M_SIZE*s_sq_T - s_T*s_T)));
+        //Correlation coefficient with magnification
+        R[j] = 20*(M_SIZE*s_HT - s_H*s_T)/(sqrt((M_SIZE*s_sq_X - s_H*s_H)*(M_SIZE*s_sq_T - s_T*s_T)));
 
         squaredSum += R[j]*R[j];
       }
-
+      printf("Squared sum of R: %f Key guess: %d\n", squaredSum, ki);
       if ((squaredSum > max_correlation)){
         keyArray[b]= (uint8_t)ki;
         max_correlation = squaredSum;
