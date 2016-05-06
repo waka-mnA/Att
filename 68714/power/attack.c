56dbb947381b9eae3a9602dba92234b480287e5c
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2016-05-06 01:58:19 +0100

    power

--- 68714/power/attack.c
+++ 68714/power/attack.c
@@ -305,13 +305,15 @@ void attack() {
     printf("Calculating intermediates ENDS.\n");
 
 
-    //Clear Index array for subset
-    memset(A_ID, 0, M_SIZE);
-    memset(B_ID, 0, M_SIZE);
-    A_NUM= 0;
-    B_NUM=0;
+
+
     int max = 0;
     for (int ki = 0;ki<BYTE;ki++){
+      //Clear Index array for subset
+      memset(A_ID, 0, M_SIZE);
+      memset(B_ID, 0, M_SIZE);
+      A_NUM= 0;
+      B_NUM=0;
       for (int i = 0;i<M_SIZE;i++){
         //devide traces by allocating index
         tracePartition(h[i][ki], i);
