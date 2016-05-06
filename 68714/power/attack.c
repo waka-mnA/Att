e4903f6a834a89759da3a707e9011af2e416b3eb
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2016-05-06 10:16:36 +0100

    power

--- 68714/power/attack.c
+++ 68714/power/attack.c
@@ -290,11 +290,11 @@ void attack() {
         for (int i = 0;i<M_SIZE;i++){
           sum_H +=abs((double)h[i][ki] - mean_H);
           sum_T +=abs((double)t[i][j] - mean_T);
+                  printf("%f %f\n", sum_H, (double)h[i][ki] - mean_H);
         }
         double s_H = sqrt(sum_H/(double)(M_SIZE-1));
         double s_T = sqrt(sum_T/(double)(M_SIZE-1));
 
-        printf("%f %f\n", sum_H, s_H);
         //Calculate Correlation coefficient
         R =0;
         for (int i = 0;i<M_SIZE;i++){
